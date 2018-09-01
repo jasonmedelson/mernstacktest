3ae942662bf7693ddc2a2c67982ea9803635c0bd
jedelson <edelson.m.jason@gmail.com>
2018-08-31 19:08:57 -0700

    reverted to passwords, no database

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-
-class App extends Component {
+ class App extends Component {
   // Initialize state
-  state = { anime: [] }
-
-  // Fetch anime after first mount
+  state = { passwords: [] }
+   // Fetch passwords after first mount
   componentDidMount() {
-    this.getAnime();
+    this.getPasswords();
   }
-
-  getAnime = () => {
-    // Get the anime and store them in state
-    fetch('/api/anime')
+   getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
       .then(res => res.json())
-      .then(anime => this.setState({ anime }));
+      .then(passwords => this.setState({ passwords }));
   }
-
-  render() {
-    const { anime } = this.state;
-
-    return (
+   render() {
+    const { passwords } = this.state;
+     return (
       <div className="App">
-        {/* Render the anime if we have them */}
-        {anime.length ? (
+        {/* Render the passwords if we have them */}
+        {passwords.length ? (
           <div>
-            <h1>Random Anime.</h1>
-            <ul className="anime">
+            <h1>5 Passwords.</h1>
+            <ul className="passwords">
               {/*
                 Generally it's bad to use "index" as a key.
                 It's ok for this example because there will always
-                be the same number of anime, and they never
+                be the same number of passwords, and they never
                 change positions in the array.
               */}
-              {anime.map((anime, index) =>
+              {passwords.map((password, index) =>
                 <li key={index}>
-                  {anime}
+                  {password}
                 </li>
               )}
             </ul>
             <button
               className="more"
-              onClick={this.getAnime}>
-              Roll Again
+              onClick={this.getPasswords}>
+              Get More
             </button>
           </div>
         ) : (
           // Render a helpful message otherwise
           <div>
-            <h1>No anime :(</h1>
+            <h1>No passwords :(</h1>
             <button
               className="more"
-              onClick={this.getAnime}>
+              onClick={this.getPasswords}>
               Try Again?
             </button>
           </div>
@@ -60,5 +55,4 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+ export default App;
--- index.js
+++ index.js
@@ -19,55 +19,45 @@ var mysql = require('mysql')
 
 const app = express();
 
+var db_config = {
+  host:     'us-cdbr-iron-east-01.cleardb.net',
+  user:     'beb3e092ffe131',
+  password: '23e76532',
+  database: 'heroku_20e37edc6a598e9',
+};
+// var connection = mysql.createConnection(db_config)
+// connection.connect()
+// var data=[];
+// connection.query('SELECT * FROM `anime` ', function (err, rows, fields) {
+//   if (err) throw err
+//   for(var anime of rows){
+//     data.push([anime.anime_title, anime.anime_rating])
+//   }
+//   // data = rows;
+  // console.log(data[1].anime_title);
+
+  // console.log(x)
+
+// connection.end()
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/anime', (req, res) => {
-  var connection = mysql.createConnection({
-    host:     'us-cdbr-iron-east-01.cleardb.net',
-    user:     'beb3e092ffe131',
-    password: '23e76532',
-    database: 'heroku_20e37edc6a598e9',
-  })
-  connection.connect()
-  var data=[];
-  connection.query('SELECT * FROM `anime` ', function (err, rows, fields) {
-    if (err) throw err
-    for(var anime of rows){
-      data.push([anime.anime_title, anime.anime_rating])
-    }
-    // data = rows;
-    // console.log(data[1].anime_title);
-    data_length = data.length
-    number = Math.floor(Math.random() * data_length);
-    console.log(data)
-    Math.floor(Math.random() * 10);
-    res.json(data[number])
-    console.log("anime sent")
-    // console.log(x)
-  })
+app.get('/api/passwords', (req, res) => {
   const count = 5;
-
-  // Generate some anime
-  // const anime = Array.from(Array(count).keys()).map(i =>
-  //   generatePassword(12, false)
-  // )
-
-  connection.end()
-  // Return them as json
-  // res.json(anime);
-
-  // console.log(`Sent ${count} anime`);
+   // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+   // Return them as json
+  res.json(passwords);
+   console.log(`Sent ${count} passwords`);
 });
-
-// The "catchall" handler: for any request that doesn't
+ // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-
-const port = process.env.PORT || 5000;
+ const port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
+ console.log(`Password generator listening on ${port}`);
